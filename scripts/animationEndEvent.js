c642453b78f041903141a1b1bf7602c1627c557d
ZanudaKutkh <126755177+ZanudaKutkh@users.noreply.github.com>
2025-05-22 12:12:12 +0400

    Delete scripts directory

--- scripts/animationEndEvent.js
+++
@@ -1,5 +0,0 @@
-const dispatchAnimationEnd = (nextScreen, prev = false, delay = 0, callback) => {
-  const animationEnd = new CustomEvent('animationEnd', { detail: { nextScreen, prev, delay, callback } })
-
-  window.dispatchEvent(animationEnd);
-}
